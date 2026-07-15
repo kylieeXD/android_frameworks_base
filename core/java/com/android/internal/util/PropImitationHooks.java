92eab7ad5a4481e2d87e0f93399e27786c645969
kylieeXD <khayloaftck@gmail.com>
2026-07-15 08:07:51 +0000

    Revert "PropImitationHooks: Spoof certified props to finsky as well"
    This reverts commit ba4e0f4214c0ec8738ec1e7087deca326435376c.
    
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -146,8 +146,8 @@ public class PropImitationHooks {
          * Set custom model for Netflix
          * Set Pixel XL for Google Photos
          */
-        if (sIsGms || sIsFinsky) {
-            setPlayIntegrityProps(context);
+        if (sIsGms) {
+            setCertifiedPropsForGms(context);
         } else if (!sStockFp.isEmpty() && packageName.equals(PACKAGE_ARCORE)) {
             dlog("Setting stock fingerprint for: " + packageName);
             setPropValue("FINGERPRINT", sStockFp);
@@ -178,7 +178,7 @@ public class PropImitationHooks {
         }
     }
 
-    private static void setPlayIntegrityProps(Context context) {
+    private static void setCertifiedPropsForGms(Context context) {
         if (sDisableGmsProps) {
             dlog("GMS prop imitation is disabled by user");
             return;
@@ -228,10 +228,10 @@ public class PropImitationHooks {
         };
 
         if (!was) {
-            dlog("Spoofing build for GMS / Finsky");
+            dlog("Spoofing build for GMS");
             setCertifiedProps();
         } else {
-            dlog("Skip spoofing build for GMS / Finsky, because GmsAddAccountActivityOnTop");
+            dlog("Skip spoofing build for GMS, because GmsAddAccountActivityOnTop");
         }
 
         try {
