93dcfe51db1b0edc48cdefb2d44d039e1227e5d1
kylieeXD <khayloaftck@gmail.com>
2026-07-15 08:08:17 +0000

    Revert "PIHooks: Dynamically spoof props for GMS"
    This reverts commit a085e0e82c3eeccef027b57490bca7556462f532.
    
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -221,7 +221,6 @@ import com.android.server.pdb.PersistentDataBlockService;
 import com.android.server.people.PeopleService;
 import com.android.server.permission.access.AccessCheckingService;
 import com.android.server.pinner.PinnerService;
-import com.android.server.custom.AttestationService;
 import com.android.server.pm.ApexManager;
 import com.android.server.pm.ApexSystemServiceInfo;
 import com.android.server.pm.BackgroundInstallControlService;
@@ -2897,11 +2896,6 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartHealthService");
             mSystemServiceManager.startService(HealthInterfaceService.class);
-            t.traceEnd();
-
-            // AttestationService
-            t.traceBegin("AttestationService");
-            mSystemServiceManager.startService(AttestationService.class);
             t.traceEnd();
 
             mSystemServiceManager.startService(GameSpaceManagerService.class);
